6cfa2c7439e8e12d1f04ece60529e5937d987ab3
官萍 <ping.guan@cyber-insight.com>
2020-12-31 09:48:22 +0800

    ssr

--- webpack10_ReactSSR/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const App = () => {
-  return <h1>服务器渲染测试</h1>;
-};
-
-export default App;
--- webpack11_ReactNextJS/demo/pages/index.js
+++
@@ -1,8 +0,0 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
-const HomePage = () => {
-  return <div>next.js</div>
-}
-
-export default HomePage
