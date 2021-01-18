afc01a6a265c9af205933111bc95f3182dcd438a
官萍 <ping.guan@cyber-insight.com>
2021-01-18 16:19:38 +0800

    test

---
+++ 手写源码/axios.js
@@ -0,0 +1,20 @@
+/**
+ * axios核心：axios(config) | axios.method(url, data, config)
+ */
+class Axios {
+    constructor() {
+
+    }
+
+    
+}
+
+export default Axios
+
+/**
+ * axios.get
+ * axios.post
+ * axios.delete
+ * axios.put
+ * axios.all
+ */
